d2b22d2166e5b0234cc4efacc009e7679151dd36
SimonB87 <buryans@gmail.com>
2020-08-05 18:38:50 +0200

    edit of task4 files

---
+++ prace4extra/vysledek/script.js
@@ -0,0 +1,6 @@
+function showImg(imgName) {
+
+  let mainImg = document.getElementById("mainImg");
+  let clickedImgPath = "img/" + imgName + ".jpg";
+  mainImg.src = clickedImgPath;
+}
---
+++ prace4hlavni/zadani/script.js
@@ -0,0 +1,2 @@
+let lightBulbOn = false;
+
